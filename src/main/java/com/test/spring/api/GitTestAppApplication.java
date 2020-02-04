d717665e55d9071aa76d31d953ebdacdba545b1d
manieshkrsharma <itzmemaniesh.asd@gmail.com>
2020-02-04 15:51:33 +0530

    git-test-app added

---
+++ src/main/java/com/test/spring/api/GitTestAppApplication.java
@@ -0,0 +1,24 @@
+package com.test.spring.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GitTestAppApplication {
+	@PostMapping("/purchase/{userName}/{amount}/{productName}")
+	public String purchase(@PathVariable String userName, @PathVariable double amount,
+			@PathVariable String productName) {
+		return "Hi " + userName + " order for " + productName + 
+				" with amount " + " stored successfully...";
+
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTestAppApplication.class, args);
+	}
+
+}
